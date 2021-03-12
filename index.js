e4f779fcf1bb2b22be0343c1ed868009aac7eff0
chensuiyi <chenbimo@gmail.com>
2021-03-12 10:36:41 +0800

    fix: 修复路由没生成的问题

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-let path = require("path");
-let fs = require("fs-extra");
-let fastGlob = require("fast-glob");
-let clearModule = require("clear-module");
+let path = require('path');
+let fs = require('fs-extra');
+let fastGlob = require('fast-glob');
+let clearModule = require('clear-module');
 class YipackPluginUniapp {
     apply(compiler) {
-        compiler.hooks.watchRun.tap("yipack-plugin-uniapp", (stats) => {
-            let files = fastGlob.sync(["**/route.js"], {
-                cwd: path.join(process.cwd(), "src", "pages"),
+        compiler.hooks.watchRun.tap('yipack-plugin-uniapp', (stats) => {
+            let files = fastGlob.sync(['**/route.js'], {
+                cwd: path.join(process.cwd(), 'src', 'pages'),
                 absolute: true,
-                onlyFiles: true,
+                onlyFiles: true
             });
             let routeArray = [];
             files.forEach((file) => {
-                let d = path.relative(path.join(process.cwd(), "src"), file).replace("route.js", "index").split(path.sep).join("/");
+                let d = path.relative(path.join(process.cwd(), 'src'), file).replace('route.js', 'index').split(path.sep).join('/');
                 clearModule(file);
-                if (d === "pages/index/index") {
-                    routeArray.unshift(require(file));
+                let pageJson = require(file);
+                if (!pageJson.path) {
+                    pageJson.path = d;
+                }
+                if (d === 'pages/index/index') {
+                    routeArray.unshift(pageJson);
                 } else {
-                    routeArray.push(require(file));
+                    routeArray.push(pageJson);
                 }
             });
-            let createPageData = require(path.join(process.cwd(), "src", "createPages.js"));
+            let createPageData = require(path.join(process.cwd(), 'src', 'createPages.js'));
             createPageData.pages = routeArray;
-            fs.writeFileSync(path.join(process.cwd(), "src", "pages.json"), JSON.stringify(createPageData));
-            console.log("===============");
-            console.log("pages.json已生成");
+            fs.writeFileSync(path.join(process.cwd(), 'src', 'pages.json'), JSON.stringify(createPageData));
+            console.log('===============');
+            console.log('pages.json已生成');
         });
     }
 }
